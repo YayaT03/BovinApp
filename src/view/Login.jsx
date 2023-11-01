280590ae77a51891dfb8415ca54e856f67fc892e
YayaT03 <dahianat45@ouylook.es>
2023-11-01 17:32:37 -0500

    dashboard

--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Login.css'
 
 const Login = () => {
   return (
     <div>
-    <div className='margen2'>
-      <form className="row g-3">
-  <div className="col-md-6">
-    <label for="inputEmail4" className="form-label">Email</label>
-    <input type="email" className="form-control" id="inputEmail4"/>
-  </div>
-  <div className="col-md-6">
-    <label for="inputPassword4" className="form-label">Password</label>
-    <input type="password" className="form-control" id="inputPassword4"/>
-  </div>
-
-  </form>
-
-
-
-
-
-    </div>
+      <div >
+        <div className="row justify-content-center imfondo  mt-5 ">
+            <div className="col-md-6 col-xl-4 ">
+              <br />
+              <div className="card mt-5" style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)' }}>
+                <div className="card-body text-center d-flex flex-column align-items-center mt">
+                  <div className="bs-icon-xl bs-icon-primary bs-icon my-4"><i className="fa-solid fa-user fa-beat fa-2xl"></i></div>
+                  <form method="post">
+                    <div className="mb-3"><input className="form-control" type="email" name="email" placeholder="Email" /></div>
+                    <div className="mb-3"><input className="form-control" type="password" name="password" placeholder="Contraseña" /></div>
+                    <div className="mb-3"><Link to="/aplication"><button className="btn btn-primary d-block w-100" > Ingresar </button></Link></div>
+                    <p>¿Olvidaste tu contraseña?</p>
+                  </form>
+                </div>
+              </div>
+              <br />
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
